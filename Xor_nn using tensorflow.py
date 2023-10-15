e1194c04a5eff2961907888654472b6ada329eac
Mahesh Raina <104516196+Friendofhumanity@users.noreply.github.com>
2023-10-15 23:23:14 +0530

    Updated Py file

--- Xor_nn using tensorflow.py
+++ Xor_nn using tensorflow.py
@@ -3,7 +3,7 @@ import numpy as np
 import tensorflow as tf
 
 
-# XOR gate inputs and outputs
+# XOR gate inputs , Can be modified according to your requirements
 inputs = np.array([[0, 0, 0],
                   [0, 0, 1],
                   [0, 1, 0],
@@ -24,15 +24,16 @@ targets = np.array([[0],
 
 
 
-# Create a sequential model
+# Creating a sequential model
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(4, activation='sigmoid', input_shape=(3,)),   # We haev used 3 inputs layers 4 hidden layers and
-                                                                         # 1 output layer 
-    tf.keras.layers.Dense(1, activation='sigmoid')
+    tf.keras.layers.Dense(4, activation='sigmoid', input_shape=(3,)),   # I have used 3 inputs layers 4 hidden layers and
+                                                                 
+    tf.keras.layers.Dense(1, activation='sigmoid')                       # 1 output layer
 ])
 
-# Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error', metrics='accuracy')
+# Compile the model 
+
+model.compile(optimizer='adam', loss='mean_squared_error', metrics='accuracy') 
 
 # Train the model
 
@@ -43,5 +44,5 @@ model.fit(inputs, targets, epochs=10000, verbose=0)  #you can set the verbose as
 predictions = model.predict(inputs)
 
 
-
+# print the predictions
 print(np.round(predictions))
